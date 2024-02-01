1d419e561ec1deb3cfc39322d2cd5b4d314f0ce5
Tunde <babatea4real@yahoo.com>
2024-02-01 21:02:30 +0100

    create a custome hook to fetch the games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -4,7 +4,7 @@ export default axios.create(
     {
         baseURL:"https://api.rawg.io/api",
         params:{
-            key: "869f60a6db5c4c34893009ca8cf39408"
+            key: "596f2949c8874426a94826e952f4f7d3"
         }
     }
 )
